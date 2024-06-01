9b3470ba3231a78f0e09702215c34abe0539eeaa
tal556 <tall5225@gmail.com>
2024-06-01 19:18:48 +0300

    add shadcn

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Button } from "@/components/ui/button"
 
 function App() {
   const [count, setCount] = useState(0)
@@ -31,6 +32,7 @@ function App() {
       <p className="text-3xl">
         Click on the Vite and React logos to learn more
       </p>
+      <Button>Click me</Button>
     </>
   )
 }
